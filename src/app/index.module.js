aa988359431e258294a76377d92869005d298112
rajkumar <rajkumar.gootam@gmail.com>
2016-08-30 16:44:27 +0530

    created yeoman generator and deleted main contoller

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('solution', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
---
+++ src/app/main/main.controller.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('solution')
+    .controller('MainController', MainController);
+
+  /** @ngInject */
+  function MainController() {
+    var vm = this;
+
+
+    }
+
+})();
