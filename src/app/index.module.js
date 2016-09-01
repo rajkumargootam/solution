9bcbd955e2f553a4f42c543d65cb11fb1c18fac3
rajkumar <rajkumar.gootam@gmail.com>
2016-09-01 17:05:51 +0530

    added material design and using that added vertical sidenav

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('solution', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr']);
+    .module('solution', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngMaterial','ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr']);
 
 })();
--- src/app/index.route.js
+++ src/app/index.route.js
@@ -11,8 +11,8 @@
       .state('home', {
         url: '/',
         templateUrl: 'app/main/main.html',
-        controller: 'MainController',
-        controllerAs: 'main'
+        controller: 'AppCtrl',
+        controllerAs: 'app'
       });
 
     $urlRouterProvider.otherwise('/');
